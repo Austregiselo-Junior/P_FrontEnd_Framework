59a2ce69f3a27a0b9fb49c5f99bb2b6cd5246096
Genyaltt Jr <genyalttjunior2014@gmail.com>
2025-11-27 23:31:26 -0300

    Land page funcional

--- Projeto-Asoftware-Angular/src/app/app.routes.ts
+++ Projeto-Asoftware-Angular/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { HomeComponent } from './components/home-component/home-component';
+import { Land } from './pages/land/land';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '', redirectTo: 'land', pathMatch: 'full'},
     {path: 'login', component: Login},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'land', component: Land}
 ];
